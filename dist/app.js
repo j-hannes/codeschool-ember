1d9276d56dfcee6f6a6115c4168ee2981da56a31
Jay Hannes <Hannes_E@gmx.de>
2015-01-30 20:18:23 +0100

    Add controller

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,33 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.route('about');
-  this.route('credits');
+  this.route('credits', {path: 'thanks'});
+});
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: '/images/logo.png',
+  time: function() {
+    return (new Date()).toDateString()
+  }.property()
+});
+
+var isClosed = function(date) {
+  var day = date.getDay();
+  var sunday = 0;
+  return day === sunday;
+};
+
+App.AboutController = Ember.Controller.extend({
+  contactName: 'Mr Foo',
+  avatar: 'images/avatar.jpg',
+  openStatus: function() {
+    var today = new Date();
+    if (isClosed(today)) {
+      return 'closed';
+    } else {
+      return 'open';
+    }
+  }.property()
 });
 
